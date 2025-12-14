e8df568de27d3dce10ad0d6776dfaeba14957ded
Alexandr <oleksandrglushen2005@gmail.com>
2025-12-14 19:10:01 +0200

    Add Dockerfile and dockerignore; update main script and dependencies

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
+import requests
 
+def get_weather() -> None:
+    API_KEY = os.getenv("API_KEY")
+
+    if not API_KEY:
+        raise ValueError("API_KEY is not provided")
+    url = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q=Paris"
+    response = requests.get(url)
+    data = response.json()
+    if "error" in data:
+        print("Error:", data["error"]["message"])
+        return
+    temp = data["current"]["temp_c"]
+    condition = data["current"]["condition"]["text"]
+    print(f"Paris/France {data} Weather: {temp} {condition}")
 
 if __name__ == "__main__":
     get_weather()
